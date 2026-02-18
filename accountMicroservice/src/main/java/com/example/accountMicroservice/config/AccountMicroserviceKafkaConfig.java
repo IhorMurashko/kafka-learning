9e6b2307f9a97cf41a82e9ca59df369229e93585
Ihor Murashko <i.murashko0911@gmail.com>
2026-02-18 15:16:41 +0100

    default handling error events behavior

--- accountMicroservice/src/main/java/com/example/accountMicroservice/config/AccountMicroserviceKafkaConfig.java
+++ accountMicroservice/src/main/java/com/example/accountMicroservice/config/AccountMicroserviceKafkaConfig.java
@@ -2,14 +2,18 @@ package com.example.accountMicroservice.config;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.Map;
 import java.util.Objects;
@@ -25,7 +29,8 @@ public class AccountMicroserviceKafkaConfig {
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
                 Objects.requireNonNull(environment.getProperty("spring.kafka.bootstrap-servers")),
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class,
+                ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class,
                 JsonDeserializer.TRUSTED_PACKAGES,
                 Objects.requireNonNull(environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages")),
                 ConsumerConfig.GROUP_ID_CONFIG,
@@ -36,11 +41,28 @@ public class AccountMicroserviceKafkaConfig {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-            ConsumerFactory<String, Object> consumerFactory
+            ConsumerFactory<String, Object> consumerFactory, KafkaTemplate kafkaTemplate
     ) {
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate));
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler);
         return factory;
     }
 
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = Map.of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                Objects.requireNonNull(environment.getProperty("spring.kafka.bootstrap-servers")),
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, String.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class
+        );
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
 }
